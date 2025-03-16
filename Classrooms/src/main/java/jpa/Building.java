392fff85777dcd1af999d09689a557c2efb15280
Nathan Lenz <nathan@freelenzer.at>
2025-03-16 16:25:49 +0100

    Datenstruktur passt - dll.drawio aktualisiert

--- Classrooms/src/main/java/jpa/Building.java
+++ Classrooms/src/main/java/jpa/Building.java
@@ -3,8 +3,6 @@ package jpa;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 
-import java.util.List;
-
 @Entity
 public class Building extends PanacheEntity {
     public String kuerzel; //standardmäßig nur ein character
