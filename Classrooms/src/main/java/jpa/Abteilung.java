ed7311631e4249e433f51f2eea5027a4dfc9715b
Nathan Lenz <nathan@freelenzer.at>
2025-03-21 13:55:09 +0100

    Fehlerkorrektur

--- Classrooms/src/main/java/jpa/Abteilung.java
+++ Classrooms/src/main/java/jpa/Abteilung.java
@@ -7,4 +7,20 @@ import jakarta.persistence.Entity;
 public class Abteilung extends PanacheEntity {
     public String nickname; //one character representation of name
     public String name;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
